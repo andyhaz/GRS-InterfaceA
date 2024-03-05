61c04b4516491629978dd226d058ee0fca1a0bf3
andrew <andyhaz1970@gmail.com>
2024-03-05 13:23:58 -0600

    Initial Commit

---
+++ GRS InterfaceA/GRS_InterfaceAApp.swift
@@ -0,0 +1,17 @@
+//
+//  GRS_InterfaceAApp.swift
+//  GRS InterfaceA
+//
+//  Created by andyhaz on 3/5/24.
+//
+
+import SwiftUI
+
+@main
+struct GRS_InterfaceAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
